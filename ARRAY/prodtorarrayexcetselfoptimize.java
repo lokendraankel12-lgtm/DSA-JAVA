2ec91d321df5ee685175ab7fd21837c5e0ea428f
Lokendra Ankel <lokendraankel12@gmail.com>
2026-07-17 23:12:34 +0530

    Create prodtorarrayexcetselfoptimize.java

---
+++ ARRAY/prodtorarrayexcetselfoptimize.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        
+        // C++ के वेक्टर्स को जावा के एरे में बदला
+        int[] ans = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        
+        // जावा में एरे के हर डिब्बे को शुरुआत में 1 से भरने के लिए
+        Arrays.fill(ans, 1);
+        Arrays.fill(prefix, 1);
+        Arrays.fill(suffix, 1);
+        
+        // 1. Prefix Loop (आगे से पीछे)
+        for(int i = 1; i < n; i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+        
+        // 2. Suffix Loop (पीछे से आगे) - दीदी ने i = n-2 से शुरू किया है
+        for(int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        
+        // 3. Final Multiplication Loop
+        for(int i = 0; i < n; i++) {
+            ans[i] = prefix[i] * suffix[i];
+        }
+        
+        return ans;
+    }
+}
