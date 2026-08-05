8ef43660f0c3eda08ab2f4a9dcfd4004b3fca210
Lokendra Ankel <lokendraankel12@gmail.com>
2026-08-05 18:45:47 +0530

    Create checkwordpalindromeornot.java

---
+++ STRING/checkwordpalindromeornot.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main {
+    public static boolean checkpalindrome(String str){
+        int n= str.length();
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+     String str="noen";
+   System.out.print(checkpalindrome( str));
+      
+    }
+     
+        }
