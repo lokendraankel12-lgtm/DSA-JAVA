4ce89a880ca972e1f21a455869777f67a3c5afaa
Lokendra Ankel <lokendraankel12@gmail.com>
2026-08-13 16:13:09 +0530

    Create find minimum size subarray sum for given target sum.java

---
+++ ARRAY/find minimum size subarray sum for given target sum.java
@@ -0,0 +1,25 @@
+// most optimize solution by using sliding window pattern which gives o(n) time complexity
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n= nums.length;
+        int low=0;
+        int high=0;
+        int sum=0;
+        int result=Integer.MAX_VALUE;
+        while(high<n){
+            sum= sum+nums[high];
+            while(sum>=target){
+                int len=high-low+1;
+                result=Math.min(result,len);
+                sum=sum-nums[low];
+                low++;
+            }
+            high++;
+        }
+        if(result==Integer.MAX_VALUE){
+            return 0;
+        }
+        return result;
+        
+    }
+}
